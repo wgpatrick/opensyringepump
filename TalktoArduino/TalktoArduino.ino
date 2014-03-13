a1c5342837a05c8f0ce79b83c07f598b1a08c6b6
Will Patrick <willpatrick@Wills-iMac-2.local>
2014-03-13 15:20:56 -0400

    updated ino. using stepper library to drive pump

--- TalktoArduino/TalktoArduino.ino
+++ TalktoArduino/TalktoArduino.ino
@@ -1,27 +1,36 @@
-#include <IEEE754tools.h>
-SoftwareSerial mySerial(0,1);
+#include <SoftwareSerial.h>
+#include <Stepper.h>
 
+SoftwareSerial mySerial(6,7);
 // Pins
 
 // Pins for serial communications
 int TX = 6;
 int RX = 7;
 
-
 // Inputs to motor driver from ATTiny
-int Input1A = ;
-int Input2A = ;
-int Input1B = ;
-int Input2B = ;
+int Input1A = 4;
+int Input2A = 5;
+int Input1B = 0;
+int Input2B = 1;
+int Steps = 200;
+Stepper myStepper = Stepper(Steps,Input1A,Input2A,Input1B,Input2B);
 
 void setup()
 {
-  pinMode(RX,INPUT);
-  pinMode(TX,OUTPUT);
-  Serial.begin(9600);
+mySerial.begin(9600);
+mySerial.println("Hello, world?");
+myStepper.setSpeed(10);
 }
-
+ 
 void loop()
 {
-  
+
+myStepper.step(Steps);
+delay(100);
+
+
+myStepper.step(-Steps);
+delay(100);
+
 }
