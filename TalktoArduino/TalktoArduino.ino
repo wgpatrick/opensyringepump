b9f5a2dcae11ab3f8be41a7f99cc25b92e34d657
Will Patrick <wgpatrick@gmail.com>
2014-04-13 19:25:08 -0400

    made a few changes to talktoarduino code

--- TalktoArduino/TalktoArduino.ino
+++ TalktoArduino/TalktoArduino.ino
@@ -1,24 +1,24 @@
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 
-SoftwareSerial mySerial(6,7);
+SoftwareSerial mySerial(0,1);
 // Pins
 
 // Pins for serial communications
-int TX = 6;
-int RX = 7;
+int TX = 1;
+int RX = 0;
 
 // Inputs to motor driver from ATTiny
-int Input1A = 4;
-int Input2A = 5;
-int Input1B = 0;
-int Input2B = 1;
-int Steps = 200;
+int Input1A = A0;
+int Input2A = A1;
+int Input1B = A4;
+int Input2B = A3;
+int Steps = 400;
 Stepper myStepper = Stepper(Steps,Input1A,Input2A,Input1B,Input2B);
 
 void setup()
 {
-mySerial.begin(9600);
+mySerial.begin(57600);
 mySerial.println("Hello, world?");
 myStepper.setSpeed(10);
 }
@@ -26,11 +26,14 @@ myStepper.setSpeed(10);
 void loop()
 {
 
+mySerial.println("Down!");
 myStepper.step(Steps);
-delay(100);
+mySerial.println("Done...");
+delay(1000);
 
-
+mySerial.println("Up!");
 myStepper.step(-Steps);
-delay(100);
+mySerial.println("Done...");
+delay(1000);
 
 }
